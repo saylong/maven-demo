296b4d67a2b5cc9685778567125cb3a60eb13916
lisailong <sailong.li@meinergy>
2020-11-06 11:06:40 +0800

    Initial commit

---
+++ src/main/java/com/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+/**
+ * @author lsl
+ * @date 2020/11/6
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("demo-start!!!!!");
+    }
+}
